dec9f4b5de7999f7f55f0d9303c8b0021da52b5d
SteveElouga <nyobeelouga5@gmail.com>
2024-12-04 12:02:29 +0100

    seconsdds commit

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask_socketio import SocketIO, emit
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '02149e48f1de528608b4bd6035b036f04f1e802f'
 cors = CORS(app, resources={r"*": {"origins": "*"}})
-socketio = SocketIO(app)
+socketio = SocketIO(app, cors_allowed_origins="*")
 
 @app.route('/')
 def index():
